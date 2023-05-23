9667ee49cf021adf9c16abfa3739ea903740a7f2
Guilherme Vasco <guilhermevasco08@gmail.com>
2023-05-23 11:02:33 -0300

    Alterações na tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,30 @@ import React from 'react';
 const Home: NextPage = () => {  
     
     return (
-        <div className="bg-BgColor w-screen h-screen">
+        <div className="global-background w-screen h-screen">
             
-            <Flex  justifyContent='center' >
-                <div>
+            <Flex  justifyContent='center'>
+                <div className="mb-8">
                     <VStack spacing='2rem'>
                         <div>
-                            <h2 className="text-4xl	font-bold text-left">Soccer App</h2>
+                            <img src="C:\Users\moham\Documents\GitHub\soccer-react-app\src\components\Images\soccer-app-logo.png" alt="Soccer App Logo" />
                         </div>
 
-                        <div className="bg-[#e4ffe6] w-1990 h-712 rounded-3xl flex flex-row">
-                            <p className="text-2xl p-5">
-                                Tela de login do site. 
-                            </p>                            
+                        <div className="flex flex-col items-center justify-center h-screen">
+                            <form className="flex flex-col w-1/10 bg-[#d3f5ff] shadow-md rounded p-8">
+                                <h1 className="text-2xl font-bold mb-5 flex items-center justify-center">Login na API</h1>
+                                <input
+                                placeholder="Token da API"
+                                className="mb-4 py-2 px-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                                />
+
+                                <button
+                                type="submit"
+                                className="bg-[#31c48d] hover:bg-[#28a074] text-white font-bold py-2 px-4 rounded"
+                                >
+                                Login
+                                </button>
+                            </form>
                         </div>                        
                     </VStack>
                 </div>
@@ -25,4 +36,5 @@ const Home: NextPage = () => {
         </div>
     );
 }
+
 export default Home
