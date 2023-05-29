9c7e62c73b166d85db33a797be022f4095db4752
Guilherme Vasco <guilhermevasco08@gmail.com>
2023-05-29 12:19:31 -0300

    Adição de tabela e estilização

--- src/pages/resultados/index.tsx
+++ src/pages/resultados/index.tsx
@@ -17,11 +17,17 @@ interface PlayerData {
     };
 }
 
-interface Lineups {
-    lineups: {
-        formation: string;
-        played: number;
-    };
+interface Goals {
+    goals: {
+        t0_15: number;
+        t16_30: number;
+        t31_45: number;
+        t46_60: number;
+        t61_75: number;
+        t76_90: number;
+        t91_105: number;
+        t106_120: number;
+    }
 }
 
 const teamDetails = () => {
@@ -29,11 +35,16 @@ const teamDetails = () => {
     const [leagueId, setleagueId] = useState<number | null>(null);
     const [token, setToken] = useState('');
     const currentYear = new Date().getFullYear();
-    const [dataTeam, setDataTeam] = useState<Team[]>([]);
     const [dataPlayers, setDataPlayers] = useState<PlayerData[]>([]);
-    const [dataLineups, setDataLineups] = useState<Lineups[]>([]);
     const [teamName, setTeamName] = useState('');
     const [mainLineup, setMainLineup] = useState('Sem informações');
+
+    const [played, setPlayed] = useState(0);
+    const [wins, setWins] = useState(0);
+    const [draws, setDraws] = useState(0);
+    const [loses, setLoses] = useState(0);
+
+    const [goals, setGoals] = useState<Goals | null>(null);
 
     useEffect(() => {
         const searchParams = new URLSearchParams(window.location.search);
@@ -66,11 +77,31 @@ const teamDetails = () => {
                 }
             });
             const data = await response.json();
-            setDataTeam(data.response.team);
-            setDataLineups(data.response.lineups);
 
             setTeamName(data.response.team.name);
             setMainLineup(data.response.lineups[0].formation);
+
+            setPlayed(data.response.fixtures.played.total);
+            setWins(data.response.fixtures.wins.total);
+            setDraws(data.response.fixtures.draws.total);
+            setLoses(data.response.fixtures.loses.total);
+
+            const goalsData = data.response.goals.for.minute;
+
+            const goals: Goals = {
+                goals: {
+                    t0_15: goalsData['0-15'].total || 0,
+                    t16_30: goalsData['16-30'].total || 0,
+                    t31_45: goalsData['31-45'].total || 0,
+                    t46_60: goalsData['46-60'].total || 0,
+                    t61_75: goalsData['61-75'].total || 0,
+                    t76_90: goalsData['76-90'].total || 0,
+                    t91_105: goalsData['91-105'].total || 0,
+                    t106_120: goalsData['106-120'].total || 0,
+                }
+            };
+
+            setGoals(goals);
         } catch (error) {
             console.error('Erro ao obter os detalhes do time:', error);
         }
@@ -98,29 +129,51 @@ const teamDetails = () => {
     }, [token, teamId]);
 
     return (
-        <div>
-            <div>
-                <h2>Informações sobre {teamName}</h2>
+        <div className="items-center justify-center h-screen team-details font-color global-background">
+            <div className='flex flex-col w-1/10 noise-background shadow-md rounded p-8'>
+                <h1 className='text-2xl font-bold flex items-center justify-center font-color'>Informações sobre {teamName}</h1>
             </div>
             <div>
-                <h2>Jogadores</h2>
-                <ul>
+                <h2 className='font-bold flex items-center justify-center font-color mt-5 flex-col w-1/10 noise-background shadow-md rounded p-8'>Jogadores</h2>
+                <ul className="player-list font-color">
                     {dataPlayers.map((player) => (
-                        <li key={player.player.id}>
-                            <div>
-                                <img src={player.player.photo} alt={player.player.name} />
-
-                                <div>
-                                    <h3>{player.player.name}</h3>
-                                    <p>Idade: {player.player.age}</p>
-                                    <p>Nacionalidade: {player.player.nationality}</p>
+                        <li key={player.player.id} className='player-item font-color'>
+                            <div className="player-info font-color">
+                                <img className="player-photo" src={player.player.photo} alt={player.player.name} />
+                                <div className="player-details font-color">
+                                <h3 className='font-bold font-color'>{player.player.name}</h3>
+                                <p>Idade: {player.player.age}</p>
+                                <p>Nacionalidade: {player.player.nationality}</p>
                                 </div>
                             </div>
                         </li>
                     ))}
                 </ul>
-                <h2>Formação favorita</h2>
-                <p>{mainLineup}</p>
+
+                <h2 className='font-bold flex items-center justify-center global-background font-color mt-5 flex-col w-1/10 noise-background shadow-md rounded p-8'>Formação favorita</h2>
+                <p className='flex items-center justify-center global-background font-color mb-5'>{mainLineup}</p>
+
+                <h2 className='font-bold flex items-center justify-center global-background font-color flex-col w-1/10 noise-background shadow-md rounded p-8'>Estatísticas de jogos</h2>
+                <table className="game-stats global-background font-color">
+                    <thead>
+                        <tr>
+                        <th>Jogos</th>
+                        <th>Vitórias</th>
+                        <th>Empates</th>
+                        <th>Derrotas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>{played}</td>
+                        <td>{wins}</td>
+                        <td>{draws}</td>
+                        <td>{loses}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <h2 className='font-bold flex items-center justify-center global-background font-color mt-5 flex-col w-1/10 noise-background shadow-md rounded p-8'>Gols por tempo de jogo</h2>
             </div>
         </div>
     );
